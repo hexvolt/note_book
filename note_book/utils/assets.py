2d946cb5f71d839e5e28dbec7b79ef452ede19df
hexvolt <hexvolt@gmail.com>
2015-02-01 20:07:34 +0200

    ticket:6 bootstrap added

--- note_book/utils/assets.py
+++ note_book/utils/assets.py
@@ -5,7 +5,14 @@ bundles = {
     'libs_js': Bundle(
         'js/lib/jquery-1.11.2.min.js',
         'js/lib/angular.min.js',
+        'js/lib/bootstrap.min.js',
         output='js/libs.min.js',
+    ),
+    'libs_css': Bundle(
+        'less/bootstrap.less',
+        'less/bootstrap-theme.less',
+        output='css/libs.min.css',
+        filters='less'
     ),
     'main_js': Bundle(
         'js/base.js',
