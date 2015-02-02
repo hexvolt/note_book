b29b2d4a20670e6fe9d07d5d276c3ae55e70725b
hexvolt <hexvolt@gmail.com>
2015-02-02 16:32:24 +0200

    ticket:6 get rid of bootstrap, implemented new styles

--- note_book/utils/assets.py
+++ note_book/utils/assets.py
@@ -5,14 +5,7 @@ bundles = {
     'libs_js': Bundle(
         'js/lib/jquery-1.11.2.min.js',
         'js/lib/angular.min.js',
-        'js/lib/bootstrap.min.js',
         output='js/libs.min.js',
-    ),
-    'libs_css': Bundle(
-        'less/bootstrap.less',
-        'less/bootstrap-theme.less',
-        output='css/libs.min.css',
-        filters='less'
     ),
     'main_js': Bundle(
         'js/base.js',
@@ -20,6 +13,8 @@ bundles = {
         output='js/main.min.js',
     ),
     'main_css': Bundle(
+        'less/variables.less',
+        'less/utils.less',
         'less/base.less',
         output='css/main.min.css',
         filters='less'
