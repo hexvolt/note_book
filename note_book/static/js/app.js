60670d068384ed64c95afbc03f01438956986334
hexvolt <hexbolt_email@email.com>
2015-02-16 09:08:54 +0200

    ticket:9 switching the tabs via angular implemented, small styles improvements

--- note_book/static/js/app.js
+++ note_book/static/js/app.js
@@ -10,3 +10,16 @@ app.controller('TimelineController', function() {
     this.articles = 'Here will be the articles';
 
 });
+
+app.controller('TabController', function() {
+    this.activeTab = 1;
+
+    this.selectTab = function(tab) {
+        this.activeTab = tab;
+    };
+
+    this.isSelected = function(tab) {
+        return this.activeTab === tab;
+    };
+
+});
